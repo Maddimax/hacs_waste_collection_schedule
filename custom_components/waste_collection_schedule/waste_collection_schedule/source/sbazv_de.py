6fa03e75247dc730faa16ecbc7ded21bbc35e8e2
Marcus Tillmanns <mti@ashampoo-systems.com>
2024-11-15 17:54:23 +0100

    Fix SBAZV Provider
    The "api" of SBAZV has changed (see #2870). I used @rmi1974
    solution and changed it so the user can simply enter the URL
    he copied from the Website.

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/sbazv_de.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/sbazv_de.py
@@ -6,12 +6,9 @@ TITLE = "Südbrandenburgischer Abfallzweckverband"
 DESCRIPTION = "SBAZV Brandenburg, Deutschland"
 URL = "https://www.sbazv.de"
 TEST_CASES = {
-    "Wildau": {"city": "wildau", "district": "Wildau", "street": "Miersdorfer Str."},
-    "Schönefeld": {
-        "city": "Schönefeld",
-        "district": "Großziethen",
-        "street": "kleistring",
-    },
+    "Wildau": {"url": "https://fahrzeuge.sbazv.de/WasteManagementSuedbrandenburg/WasteManagementServiceServlet?ApplicationName=Calendar&SubmitAction=sync&StandortID=1369739001&AboID=10760&Fra=P;R;WB;L;GS"},
+
+    # Additional test cases can be added here
 }
 
 ICON_MAP = {
@@ -22,45 +19,39 @@ ICON_MAP = {
     "Weihnachtsbäume": "mdi:pine-tree",
 }
 
-# _LOGGER = logging.getLogger(__name__)
-
-PARAM_TRANSLATIONS = {
-    "de": {
-        "city": "Ort",
-        "district": "Ortsteil",
-        "street": "Straße",
-    },
-}
-
-
 class Source:
-    def __init__(self, city, district, street=None):
-        self._city = city
-        self._district = district
-        self._street = street
+    def __init__(self, url):
+        self.url = url
         self._ics = ICS()
 
     def fetch(self):
-        args = {
-            "city": self._city,
-            "district": self._district,
-            "street": self._street,
+        standort_id = self.url.split("StandortID=")[1].split("&")[0]
+        abo_id = self.url.split("AboID=")[1].split("&")[0]
+
+        url = "https://fahrzeuge.sbazv.de/WasteManagementSuedbrandenburg/WasteManagementServiceServlet"
+        params = {
+            "ApplicationName": "Calendar",
+            "SubmitAction": "sync",
+            "StandortID": standort_id,
+            "AboID": abo_id,
+            "Fra": "P;R;WB;L;GS"
         }
 
-        # get ics file
-        # https://www.sbazv.de/entsorgungstermine/klein.ics?city=Wildau&district=Wildau&street=Miersdorfer+Str.
-        r = requests.get(
-            "https://www.sbazv.de/entsorgungstermine/klein.ics", params=args
-        )
+        # GET request to retrieve ICS file
+        r = requests.get(url, params=params)
+        
+        # Check if the response is successful
+        if r.status_code != 200:
+            raise ValueError(f"Error fetching ICS data. Status code: {r.status_code}")
 
-        # parse ics file
+        # Parse ICS data
         dates = self._ics.convert(r.text)
 
         entries = []
         for d in dates:
             waste_type = d[1].strip()
             next_pickup_date = d[0]
-            # remove duplicates
+            # Remove duplicates
             if any(
                 e.date == next_pickup_date and e.type == waste_type for e in entries
             ):
--- doc/source/sbazv_de.md
+++ doc/source/sbazv_de.md
@@ -2,6 +2,14 @@
 
 Support for schedules provided by [https://www.sbazv.de/](https://www.sbazv.de/).
 
+
+## Configure via UI
+
+* Go to https://www.sbazv.de/entsorgungstermine/restmuell-papier-gelbesaecke-laubsaecke-weihnachtsbaeume/
+* Fill in the address
+* Click "URL in die Zwischenablage kopieren"
+* Copy it into the "url" field
+
 ## Configuration via configuration.yaml
 
 ```yaml
@@ -9,20 +17,12 @@ waste_collection_schedule:
   sources:
     - name: sbazv_de
       args:
-        city: CITY
-        district: DISTRICT
-        street: STREET
+        url: URL_COPIED_FROM_WEBSITE
 ```
 
 ### Configuration Variables
 
-**city**  
-*(string) (required)*
-
-**district**  
-*(string) (required)*
-
-**street**  
+**url**  
 *(string) (required)*
 
 ## Example
@@ -32,19 +32,6 @@ waste_collection_schedule:
   sources:
     - name: sbazv_de
       args:
-        city: "wildau"
-        district: "wildau"
-        street: "Ahornring"
+        urL: https://fahrzeuge.sbazv.de/WasteManagementSuedbrandenburg/WasteManagementServiceServlet?ApplicationName=Calendar&SubmitAction=sync&StandortID=1369739001&AboID=10760&Fra=P;R;WB;L;GS
 ```
 
-## How to get the source arguments
-
-### Hardcore Variant: Extract arguments from website
-
-The way to get the source arguments is to extract them from the website using a (desktop) browser with developer tools, e.g. Google Chrome:
-
-1. Open [https://www.sbazv.de/entsorgungstermine/restmuell-papier-gelbesaecke-laubsaecke-weihnachtsbaeume/](https://www.sbazv.de/entsorgungstermine/restmuell-papier-gelbesaecke-laubsaecke-weihnachtsbaeume/).
-2. Enter your data and click on "Termine anzeigen"
-3. Right-click on "Download als ICS Datei"
-4. In the context menu click on "Copy link address" (or something similiar, depending on your browser).
-5. Paste the copied string to a text editor. You can see the string to download your ics file now.
